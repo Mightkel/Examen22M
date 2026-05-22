0aa41c352164c0ac2bb10de72dd04a160be21412
Maykel <maykelcruz@uamv.edu.ni>
2026-05-22 09:20:54 -0600

    feat: configuracion

--- Examen/Program.cs
+++ Examen/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Una tienda de tecnología desea registrar las ventas realizadas durante una semana.
+//El sistema será desarrollado en equipos de 4 estudiantes utilizando arreglos
+//mostrar menu para registrar, mostrar productos, calcular total, mostrar mas vendido, mostrar promedio y salir
+//utilizar una matriz para mostrar todos los productos registrados junto con su precio y cantidad en una tabla
+//todo debe resolverse utilizando arreglos
