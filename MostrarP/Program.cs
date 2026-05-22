a5886ecb93f15c509767ad83987af943b17c5b3a
JaBaca67 <jabaca@uamv.edu.ni>
2026-05-22 10:35:10 -0600

    Mostrar el producto mas vendido

--- MostrarP/Program.cs
+++ MostrarP/Program.cs
@@ -1,27 +1,25 @@
-﻿//Mostrar todos los productos registrados, dentro de la matriz, donde la primera columna es el nombre del   producto, la segunda columna es el precio del producto y la tercera columna es la cantidad del producto.
+﻿﻿int[] cantidades = new int[10];
+double[] precios = new double[10];
+string[] productos = new string[10];
+int contador = 0;
+double total = 0;  
 
+        case 4:
+         if (contador == 0)
+        {
+            Console.WriteLine("No se ingresaron productos.");
+            break;
+        }   
 
-using System;
-using System.Collections.Generic;   
-
-for (int i = 0; i < 3; i++)
-{
-    Console.WriteLine("Ingrese el nombre del producto:");
-    string nombreProducto = Console.ReadLine();
-
-    Console.WriteLine("Ingrese el precio del producto:");
-    decimal precioProducto = decimal.Parse(Console.ReadLine());
-
-    Console.WriteLine("Ingrese la cantidad del producto:");
-    int cantidadProducto = int.Parse(Console.ReadLine());
-
-    // Aquí puedes almacenar los datos en una matriz o lista según tus necesidades
-    // Por ejemplo, podrías usar una lista de objetos para almacenar los productos
-}
-
-
-for (int i = 0; i < 3; i++)
-{
-    // Aquí puedes mostrar los productos almacenados en la matriz o lista
-    // Por ejemplo, si usaste una lista de objetos, podrías iterar sobre ella para mostrar los productos
-}
+            int maxCantidad = cantidades[0];   
+            int indiceMax = 0;
+            for (int i = 1; i < contador; i++)
+            {
+                if (cantidades[i] > maxCantidad)
+                {
+                    maxCantidad = cantidades[i];
+                    indiceMax = i;
+                }
+            }
+            Console.WriteLine($"Producto más vendido: {productos[indiceMax]} con {cantidades[indiceMax]} unidades vendidas.");
+        break;
